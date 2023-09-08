01911ba715a03932e26ac5c874417be87af03281
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-08 21:26:17 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,8 +1,5 @@
-#creating user data data.sh
-#! /bin/bash
 sudo yum install -y git 
-sudo git clone https://github.com/GOUSERABBANI44/Portfolio.git
-cd Portfolio
-sudo yum install -y python3-pip
+sudo git clone https://github.com/GOUSERABBANI44/flight-perdiction.git
+cd flight-perdiction
 pip3 install -r requirements.txt
 python3 app.py
