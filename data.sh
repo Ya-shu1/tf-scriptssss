e55369b584ac4ee244f5a1e00ad6a86dec241c02
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-10 12:32:11 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 sudo yum update -y
 sudo yum install -y git 
-sudo git clone https://github.com/GOUSERABBANI44/flight-perdiction.git
+sudo git clone https://github.com/ajay77777777/flight-perdiction.git
 cd flight-perdiction
 sudo pip3 install -r requirements.txt
 python3 app.py
