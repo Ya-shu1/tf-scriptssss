0d9c39e1a99dc59ff3d8eb1d3574414491ce5857
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-14 13:10:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -11,3 +11,15 @@ tags={
 Name="my public insatance1"
 }
 }
+resource "aws_instance""instance1"{
+ami="ami-0f409bae3775dc8e5"
+instance_type="t2.micro"
+key_name="vijay"
+vpc_security_group_ids=["${aws_security_group.demosg.id}"]
+subnet_id=aws_subnet.public-subnet-2.id
+associate_public_ip_address=true
+user_data=file("data2.sh")
+tags={
+Name="my public insatance1"
+}
+}
