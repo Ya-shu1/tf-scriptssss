8598f72912acf0d07b903a2c20ca0e09bbcda1b7
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-14 13:16:28 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -8,3 +8,12 @@
     Name = "Web Subnet 1"
   }
 }
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id                  = aws_vpc.demovpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "Web Subnet 1"
+  }
+}
