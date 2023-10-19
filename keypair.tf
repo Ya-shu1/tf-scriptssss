b539ebec607ac2869abd73655e34b3a6e0d3a046
Ya-shu1 <103036816+Ya-shu1@users.noreply.github.com>
2023-10-19 14:56:47 +0530

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,13 +1,1 @@
-#creating key pair key.tf
-resource "aws_key_pair" "vijay" {
-key_name = "vijay"
-public_key = tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-algorithm = "RSA"
-rsa_bits =4096
-}
-resource "local_file" "tf-key" {
-content = tls_private_key.rsa.private_key_pem
-filename = "vijay"
-}
+
